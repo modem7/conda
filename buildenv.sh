5373aef0ecb02fd3c73d7a4b0be100f8e6147343
modem7 <modem7@gmail.com>
2022-08-03 10:37:21 +0100

    update

--- buildenv.sh
+++ buildenv.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
+# Set variables
 CONDAENV=test
-CHANNEL1=default
-CHANNEL2=conda-forge
-CHANNEL3=anaconda
+CHANNEL1=default CHANNEL2=conda-forge CHANNEL3=anaconda CHANNEL4=intel
 
 # Create environment
-mamba create --name $CONDAENV -y --file /data/conda-requirements.txt -c $CHANNEL1 -c $CHANNEL2 -c $CHANNEL3
+mamba create --name $CONDAENV -y --file /data/conda-requirements.txt -c $CHANNEL1 -c $CHANNEL2 -c $CHANNEL3 -c $CHANNEL4
 
 # Initialise Shell
 conda init bash
 
 # Reload Shell
-#bash ~/.bashrc && . ~/.bashrc && source ~/.bashrc
 source ~/.bashrc
 
 # Activate environment and install additional packages
@@ -24,9 +22,7 @@ python3 -m pip install -U -r ./pip-requirements.txt
 mamba env export --no-builds > ./environment.yml
 
 # Install additional packages that conflict otherwise
-mamba install -y -c $CHANNEL2 -c $CHANNEL3 -c $CHANNEL1 libgfortran5==9.3.0
-
-
+mamba install -y -c $CHANNEL2 libgfortran5==9.3.0
 
 echo "Created environment file"
 exit
