0470e6dc987bd21fcff179416f2a5027c96fb7f5
Alex <modem7@gmail.com>
2022-08-17 14:28:52 +0100

    Updating

---
+++ tensorflow_lite/tflitefullbuild/tfilte_docker_base/buildenv-initialise_aggregate.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "### Initialising dep build..."
+echo "### Please be patient, this can take 15-20 minutes..."
+
+# Global Envs
+SECONDS="0"
+IMGNAME="condaforge/mambaforge-pypy3:4.13.0-1"
+export CONDAENV="tensorflow"
+export PYTHONENV="3.7"
+export CONDARC="/opt/conda/.condarc"
+VOLUME=CONDACACHE
+
+# Image Specific Envs
+# Aggregate
+export CONDAREQSCONNECT="/data/env_scripts/dependencies_connect.txt"
+export CONDAREQSDEPLOY="/data/env_scripts/dependencies_deploy.txt"
+export CONDAREQSML="/data/env_scripts/dependencies_ml.txt"
+export ENVFILEAGG="/data/environment_aggregate.yml"
+
+# Generate Environment files
+docker run --rm -v $VOLUME:/opt/conda/pkgs:cached -v "$(pwd)":/data -e CONDAENV -e PYTHONENV -e CONDARC -e CONDAREQSCONNECT -e CONDAREQSDEPLOY -e CONDAREQSML -e ENVFILEAGG $IMGNAME /bin/bash -c "/data/env_scripts/buildenv_aggregate.sh"
+
+echo ""
+echo "###################"
+date -ud "@$SECONDS" "+Time taken to build all environments: %H:%M:%S"
+echo "###################"
+echo "You can remove the conda cache with: 'docker volume rm $VOLUME'. It will be recreated at next run."
+echo "###################"
+
+exit 0
---
+++ tensorflow_lite/tflitefullbuild/tfilte_docker_base/env_scripts/buildenv_aggregate.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Set system variables
+echo "### Setting system variables..."
+SECONDS="0"
+
+# Create .condarc file
+echo "### Creating .condarc..."
+cat > $CONDARC << EOF
+channels:
+  - anaconda
+  - defaults
+EOF
+
+# Create trap for SIGINT
+trap 'kill -TERM $PID' TERM INT
+# Create environment
+echo "### Creating environment..."
+mamba create -v --no-deps --name $CONDAENV -y --file=$CONDAREQSCONNECT --file=$CONDAREQSDEPLOY --file=$CONDAREQSML python=$PYTHONENV &
+PID=$!
+wait $PID
+trap - TERM INT
+wait $PID
+EXIT_STATUS=$?
+
+# # Initialise Shell
+# echo "### Initialising shell..."
+# mamba init bash
+
+# # Reload Shell
+# echo "### Reloading shell..."
+# source ~/.bashrc
+
+# # Activate environment and install additional packages
+# echo "### Activating environment..."
+# source activate $CONDAENV
+# echo ""
+
+# Export environment file
+echo "### Exporting Conda env file..."
+mamba env export --no-builds -n $CONDAENV > $ENVFILEAGG
+
+echo ""
+echo "### Created environment file in $ENVFILEML"
+echo ""
+date -ud "@$SECONDS" "+Time taken to run script: %H:%M:%S"
+
+exit 0
