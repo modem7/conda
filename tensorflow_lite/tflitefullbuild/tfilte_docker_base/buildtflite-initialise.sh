04f1b52b190f61e39e7e38ee1806b86e65d53c06
Alex <modem7@gmail.com>
2022-08-22 20:49:21 +0100

    Updating

--- tensorflow_lite/tflitefullbuild/tfilte_docker_base/buildtflite-initialise.sh
+++ tensorflow_lite/tflitefullbuild/tfilte_docker_base/buildtflite-initialise.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
@@ -8,33 +8,40 @@ set -e
 echo "### Initialising dep build..."
 
 # Global Envs
-SECONDS="0"
-TAG_IMAGE="debian:buster"
-USER="$(id -u)"
-GROUP="$(id -g)"
+SECONDS=0
+IMAGE=debian:buster-slim
+GIT_DIR=$(pwd)
 PYTHON=python3.7
 TENSORFLOW_TARGET=native # (empty or native)
-OUT_DIR="$(pwd)/wheel"
-TENSORFLOW_DIR="$(pwd)/tensorflow"
+OUT_DIR=wheel
+TENSORFLOW_DIR=tensorflow
+APT_INSTALL="apt-get update && apt-get install -y debhelper dh-python pybind11-dev python3-all python3-setuptools python3-wheel python3-numpy python3-pip libpython3-dev zlib1g-dev curl unzip git"
+PIP_INSTALL="python3 -m pip install -U pip && python3 -m pip install -U pybind11"
 
-mkdir -p $(OUT_DIR)
-docker run --user $USER:$GROUP \
-	--rm --interactive $(shell tty -s && echo --tty) \
-	--env "PYTHON=$(PYTHON)" \
-	--env "TENSORFLOW_TARGET=$(TENSORFLOW_TARGET)" \
-	--volume $(TENSORFLOW_DIR):/tensorflow \
-	--volume $(OUT_DIR):/out \
-	$(TAG_IMAGE) \
-	/bin/bash -c "/tensorflow/tensorflow/lite/tools/pip_package/build_pip_package.sh && \
-	              (cp /tensorflow/tensorflow/lite/tools/pip_package/gen/tflite_pip/*.deb \
-	                  /tensorflow/tensorflow/lite/tools/pip_package/gen/tflite_pip/${PYTHON}/dist/{*.whl,*.tar.gz} \
-	                  /out 2>/dev/null || true)"
+echo "Making directory"
+mkdir -p $OUT_DIR
+
+echo "Running image"
+docker run \
+    --rm -it \
+    -e PYTHON="$PYTHON" \
+    -e TENSORFLOW_TARGET="$TENSORFLOW_TARGET" \
+    -v "$GIT_DIR/$TENSORFLOW_DIR":/tensorflow \
+    -v "$GIT_DIR/$OUT_DIR":/out \
+     $IMAGE /bin/bash -c "$APT_INSTALL && $PIP_INSTALL && /tensorflow/tensorflow/lite/tools/pip_package/build_pip_package.sh && \
+                         (cp /tensorflow/tensorflow/lite/tools/pip_package/gen/tflite_pip/*.deb \
+                         /tensorflow/tensorflow/lite/tools/pip_package/gen/tflite_pip/${PYTHON}/dist/{*.whl,*.tar.gz} \
+                        /out 2>/dev/null || true)"
 
 echo ""
 echo "###################"
-date -ud "@$SECONDS" "+Time taken to build ML environment: %H:%M:%S"
+echo "Deleting tar files"
+rm -vf "$GIT_DIR/$OUT_DIR"/*.gz
 echo "###################"
-echo "You can remove the conda cache with: 'docker volume rm $VOLUME'. It will be recreated at next run."
+
+echo ""
+echo "###################"
+date -ud "@$SECONDS" "+Time taken to build TFLite: %H:%M:%S"
 echo "###################"
 
 exit 0
