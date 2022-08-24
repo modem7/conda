5fd5d43e3e95e9962e958e6cfa938afe657928be
Alex <modem7@gmail.com>
2022-08-24 01:29:31 +0100

    Updating

--- tensorflow_lite/tflitefullbuild/tfilte_docker_base/entrypoint.sh
+++ tensorflow_lite/tflitefullbuild/tfilte_docker_base/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash --login
 
+set -e
+
 # https://github.com/conda-forge/docker-images/blob/main/scripts/entrypoint
 
 source /venv/bin/activate
 
 # Run whatever the user wants.
-echo "Environment Activated"
-echo ""
-echo -e "Current environment is \n$PATH"
+echo -e "\n\e[4m### Environment Activated ###\e[0m"
+echo -e "$PATH\n"
+echo -e "Python Version: $(python --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')\n"
 
 # echo "Import Numpy"
 # python -c "import numpy; print('success')"
