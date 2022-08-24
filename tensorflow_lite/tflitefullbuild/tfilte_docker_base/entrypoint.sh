07a25b338d083a41a253232c55e92065cd7710d9
Alex <modem7@gmail.com>
2022-08-24 08:00:51 +0100

    Updating

--- tensorflow_lite/tflitefullbuild/tfilte_docker_base/entrypoint.sh
+++ tensorflow_lite/tflitefullbuild/tfilte_docker_base/entrypoint.sh
@@ -11,6 +11,7 @@ echo -e "\n\e[4m### Environment Activated ###\e[0m"
 echo -e "$PATH\n"
 echo -e "Python Version: $(python --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')\n"
 
+# Add custom commands here:
 # echo "Import Numpy"
 # python -c "import numpy; print('success')"
 # echo ""
