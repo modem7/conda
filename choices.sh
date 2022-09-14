f61537e4651b978a381492dc387bc9a7e772eea3
Alex <modem7@gmail.com>
2022-09-14 02:16:26 +0100

    Created multi-choice script

---
+++ choices.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+PS3='Choose an option: '
+options=("Start" "Recreate" "Stop" "Top" "List Running" "Clear Cache" "Quit")
+select opts in "${options[@]}"; do
+    case $opts in
+        "Start")
+            echo "Creating and starting containers"
+	    docker compose -p conda up -d --remove-orphans
+            break
+            ;;
+        "Recreate")
+            echo "Recreating and rebuilding images and containers."
+	    docker compose build
+            break
+            ;;
+        "Stop")
+            echo "Stopping and removing containers and images."
+	    docker compose down --rmi all
+	    break
+            ;;
+        "Top")
+            echo "Displaying running processes."
+            docker compose top
+            break
+            ;;
+        "List Running")
+            echo "Displaying containers."
+            docker compose ps
+            break
+            ;;
+        "Clear Cache")
+            echo "Clearing build cache."
+            docker builder prune -af
+            break
+            ;;
+	"Quit")
+	    echo "Exiting script"
+	    exit
+	    ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
